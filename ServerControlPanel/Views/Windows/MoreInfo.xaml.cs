b9a4b47b92c522764dbec35d1643ab7134f3ffe2
Kasper <kasperwissendorf2011@gmail.com>
2021-03-19 09:13:18 +0100

    Update MoreInfo.xaml.cs

--- ServerControlPanel/Views/Windows/MoreInfo.xaml.cs
+++ ServerControlPanel/Views/Windows/MoreInfo.xaml.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using ServerControlPanel.Components;
 
 namespace ServerControlPanel.Views.Windows
 {
-    /// <summary>
-    /// Interaction logic for MoreInfo.xaml
-    /// </summary>
     public partial class MoreInfo : Window
     {
-        public MoreInfo()
+        public MoreInfo(Server Source)
         {
             InitializeComponent();
+
+            Title = Source.ServerId;
+            serverName.Text = Source.ServerId;
+            IP.Text = Source.IP;
+            Uptime.Text = Source.Uptime;
+            DiskSpace.Text = Source.Disk;
+            RAM.Text = Source.RAM;
+            CPU.Text = Source.CPU;
         }
     }
 }
