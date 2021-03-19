232f22dd3f0c5fdc14a0aa85961c85b04fbd7055
Kasper <kasperwissendorf2011@gmail.com>
2021-03-19 09:12:12 +0100

    Update Server.cs
    Added uptime, made username and password private, and added reload server.

--- ServerControlPanel/Components/Server.cs
+++ ServerControlPanel/Components/Server.cs
@@ -1,6 +1,7 @@
 ﻿using Renci.SshNet;
 using System;
 using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Media;
 
 
@@ -9,20 +10,27 @@ namespace ServerControlPanel.Components
     public class Server
     {
         public static List<Server> serverStats = new List<Server>();
-
-        public string ServerId { get; set; }
-        public Brush StatusColor { get; set; }
-        public string IP { get; set; }
-        public string Port { get; set; }
-
         
+        private string serverid;
+        private Brush statuscolor;
+        private string ip;
+        private string port;
+        private string username;
+        private string password;
+
+        public string ServerId { get => serverid; set => serverid = value; }
+        public Brush StatusColor { get => statuscolor; set => statuscolor = value; }
+        public string IP { get => ip; set => ip = value; }
+        public string Port { get => port; set => port = value; }
+        public string Username { set => username = value; }
+        public string Password { set => password = value; }
+
         public string Uptime { get; set; }
         public string Disk { get; set; }
         public string RAM { get; set; }
         public string CPU { get; set; }
 
-
-        public static void AddServer(string serverid, bool statuscolor, string ip, string port, SshClient client)
+        public static void AddServer(string serverid, bool statuscolor, string ip, string port, string username, string password, SshClient client)
         {
             serverStats.Add(new Server() 
             { 
@@ -30,16 +38,31 @@ namespace ServerControlPanel.Components
                 StatusColor = Status(statuscolor), 
                 IP = ip, 
                 Port = port,
-                Uptime = "NONE",
+                Username = username,
+                Password = password,
+                Uptime = UptimeTime(client),
                 Disk = DiskSpaceAvailable(client),
                 RAM = string.Format(CheckUsage(client.RunCommand("ps -eo %mem").Result) + "%"),
                 CPU = string.Format(CheckUsage(client.RunCommand("ps -eo %cpu").Result) + "%"),
-            });;
+            });
         }
 
         private static Brush Status(bool connected)
         {
             return (connected) ? Brushes.Green : Brushes.Gray;
+        }
+
+        private static string UptimeTime(SshClient client)
+        {
+            double secs = double.Parse(client.RunCommand("cat /proc/uptime").Result.Split(' ')[1]);
+            TimeSpan t = TimeSpan.FromSeconds(secs);
+
+            string answer = string.Format("{0:D2} hours, {1:D2} minutes, {2:D2} seconds",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds
+                            );
+            return answer;
         }
 
         private static string DiskSpaceAvailable(SshClient client)
@@ -57,6 +80,35 @@ namespace ServerControlPanel.Components
                 Usage += double.Parse(processUsage[i]);
             }
             return Usage;
+        }
+
+
+        public static void ReloadServers()
+        {
+
+            foreach (Server server in serverStats)
+            {
+                try
+                {
+                    using (var client = new SshClient(server.IP, int.Parse(server.Port), server.username, server.password))
+                    {
+                        client.Connect();
+
+                        server.StatusColor = Status(client.IsConnected);
+                        server.Uptime = UptimeTime(client);
+                        server.Disk = DiskSpaceAvailable(client);
+                        server.RAM = string.Format(CheckUsage(client.RunCommand("ps -eo %mem").Result) + "%");
+                        server.CPU = string.Format(CheckUsage(client.RunCommand("ps -eo %cpu").Result) + "%");
+
+                        client.Disconnect();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"The {server.serverid} could not be found!");
+                    server.StatusColor = Brushes.Gray;
+                }
+            }
         }
     }
 
